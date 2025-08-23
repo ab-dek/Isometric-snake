d2c3d49146cb18acdb3e0c63714acf3b3585383a
abenidek <abenezerd422@gmail.com>
2025-08-23 23:55:27 +0300

    render order issue fixed

--- src/game.c
+++ src/game.c
@@ -1,6 +1,7 @@
 #include "game.h"
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
+#include <stdio.h>
 
 // isometric grid transformation matrix
 const float MATRIX[2][2] = {
@@ -26,8 +27,17 @@ Sprite tailSpr;
 Apple apple;
 float timer; //used to control speed of the game
 
+objWrapper* objects; // list of game objects to be sorted and rendered
+size_t count = 0; //number of items in objects
+size_t capacity = 4; //memory allocated
 
 void init() {
+    objects = (objWrapper*)malloc(capacity*sizeof(objWrapper));
+    if (objects == NULL) {
+        perror("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+
     texture = LoadTexture("../res/sprite.png");
 
     floorSpr = (Sprite){{32, 128, 32, 32}, {0, 0, TILE_WIDTH, TILE_HEIGHT}};
@@ -35,7 +45,9 @@ void init() {
     apple = (Apple){(Sprite){{32, 192, 32, 32}, {0, 0, TILE_WIDTH, TILE_HEIGHT}}, (Vector2){10, 10}};
     snake = (Snake){(Sprite){{0, 0, 32, 32}, {0, 0, TILE_WIDTH, TILE_HEIGHT}}, (Vector2){0, 0}};
     
-    addTail(); addTail(); addTail(); // setting the initial length to 3
+    addToList(&snake, SNAKE);
+    addToList(&apple, APPLE);
+    addSegment(); addSegment(); addSegment(); // setting the initial length to 3
 
     verticalOffset = (GetScreenHeight() - TILES_Y*TILE_HEIGHT/2)/2;
     horizontalOffset = GetScreenWidth()/2 - TILE_WIDTH/2;
@@ -62,8 +74,8 @@ void updateGame() {
     float deltaTime = GetFrameTime();
     timer += deltaTime;
     if (timer >= GAME_SPEED) { 
-        addTail();
-        removeTail();
+        addSegment();
+        removeSegment();
         switch (snake.dir) {
         case UP: snake.pos.y--; break;
         case DOWN: snake.pos.y++; break;
@@ -72,6 +84,9 @@ void updateGame() {
         default: break;
         }
         timer = 0;
+        if(count > 1){
+            qsort(objects, count, sizeof(objWrapper), compare);
+        }
     }
     snake.sprite.src.x = snake.dir*32; //updating Snake facing direction 
 }
@@ -86,23 +101,32 @@ void drawGame() {
         }
     }
 
-    snake.sprite.dest.x = (MATRIX[0][0] * snake.pos.x) + (MATRIX[0][1] * snake.pos.y) + horizontalOffset;
-    snake.sprite.dest.y = (MATRIX[1][0] * snake.pos.x) + (MATRIX[1][1] * snake.pos.y) + verticalOffset;
-
-    apple.sprite.dest.x = (MATRIX[0][0] * apple.pos.x) + (MATRIX[0][1] * apple.pos.y) + horizontalOffset;
-    apple.sprite.dest.y = (MATRIX[1][0] * apple.pos.x) + (MATRIX[1][1] * apple.pos.y) + verticalOffset;
-
-    Segment* current = snake.head;
-    while(current != NULL) {
-        //TODO: rendering order!
-        tailSpr.dest.x = (MATRIX[0][0] * current->pos.x) + (MATRIX[0][1] * current->pos.y) + horizontalOffset;
-        tailSpr.dest.y = (MATRIX[1][0] * current->pos.x) + (MATRIX[1][1] * current->pos.y) + verticalOffset;
-        DrawTexturePro(texture, tailSpr.src, tailSpr.dest, (Vector2){0, 0}, 0, WHITE);
-        current = current->next;
+    for (int i = 0; i < count; i++) {
+        objWrapper obj = objects[i];
+        switch (obj.type) {
+            case SNAKE: {
+                Snake* snake = (Snake*)obj.obj;
+                snake->sprite.dest.x = isoToScreen(snake->pos).x;
+                snake->sprite.dest.y = isoToScreen(snake->pos).y;
+                DrawTexturePro(texture, snake->sprite.src, snake->sprite.dest, (Vector2){0, 0}, 0, WHITE);
+                break;
+            }
+            case SEGMENT: {
+                Segment* seg = (Segment*)obj.obj;
+                tailSpr.dest.x = isoToScreen(seg->pos).x;
+                tailSpr.dest.y = isoToScreen(seg->pos).y;
+                DrawTexturePro(texture, tailSpr.src, tailSpr.dest, (Vector2){0, 0}, 0, WHITE);
+                break;
+            }
+            case APPLE: {
+                Apple* apple = (Apple*)obj.obj;
+                apple->sprite.dest.x = isoToScreen(apple->pos).x;
+                apple->sprite.dest.y = isoToScreen(apple->pos).y;
+                DrawTexturePro(texture, apple->sprite.src, apple->sprite.dest, (Vector2){0, 0}, 0, WHITE);
+                break;
+            }
+        }
     }
-
-    DrawTexturePro(texture, apple.sprite.src, apple.sprite.dest, (Vector2){0, 0}, 0, WHITE);
-    DrawTexturePro(texture, snake.sprite.src, snake.sprite.dest, (Vector2){0, 0}, 0, WHITE);
 
     DrawText(TextFormat("Mouse Position: (%.0f, %.0f)", GetMousePosition().x - horizontalOffset, GetMousePosition().y - verticalOffset), 10, 10, 20, DARKGRAY);
     DrawText(TextFormat("Mouse Position trans: (%.0f, %.0f)", INV_MATRIX[0][0]*(GetMousePosition().x-horizontalOffset) + INV_MATRIX[0][1]*(GetMousePosition().y-verticalOffset)-1, INV_MATRIX[1][0]*(GetMousePosition().x-horizontalOffset) + INV_MATRIX[1][1]*(GetMousePosition().y-verticalOffset)), 10, 30, 20, DARKGRAY);
@@ -116,9 +140,18 @@ void drawGameover() {
 
 }
 
-static void addTail() {
+static Vector2 isoToScreen(Vector2 isoPos) {
+    int x = (MATRIX[0][0] * isoPos.x) + (MATRIX[0][1] * isoPos.y) + horizontalOffset;
+    int y = (MATRIX[1][0] * isoPos.x) + (MATRIX[1][1] * isoPos.y) + verticalOffset;
+    return (Vector2){x, y};
+}
+
+static void addSegment() { // adds a new segment in front
     Segment* newSeg = (Segment*)malloc(sizeof(Segment));
-    assert(newSeg != NULL);
+    if (newSeg == NULL) {
+        perror("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
     newSeg->pos.x = snake.pos.x;
     newSeg->pos.y = snake.pos.y;
 
@@ -130,9 +163,11 @@ static void addTail() {
         snake.head->prev = newSeg;
         snake.head = newSeg;
     }
+
+    addToList(newSeg, SEGMENT);
 }
 
-static void removeTail() {
+static void removeSegment() { // removes the last segment
     if (snake.head == snake.tail) { // Snake has only one segment
         free(snake.head);
         snake.head = NULL;
@@ -141,6 +176,65 @@ static void removeTail() {
     }
     Segment* lastSeg = snake.tail->prev; // second to last segment
     free(snake.tail);
+    removeFromList(snake.tail);
     snake.tail = lastSeg;
     snake.tail->next = NULL;
+}
+
+static void addToList(void* newObj, objType type) { // add an object to the objects array
+    if(count == capacity) {
+        capacity = capacity*2;
+        objWrapper* temp = realloc(objects, capacity*sizeof(objWrapper));
+        if (temp == NULL) {
+            perror("Failed to reallocate memory");
+            exit(EXIT_FAILURE);
+        }
+        objects = temp;
+    }
+
+    objWrapper obj;
+    obj.type = type;
+    obj.obj = newObj;
+    switch(type) {
+        case SNAKE: obj.pos = &(((Snake*)newObj)->pos); break;
+        case SEGMENT: obj.pos = &(((Segment*)newObj)->pos); break;
+        case APPLE: obj.pos = &(((Apple*)newObj)->pos); break;
+    }
+    objects[count] = obj;
+    count++;
+}
+
+static void removeFromList(void* obj) {
+    int i, isFound = -1;
+
+    for (i = 0; i < count; i++) {
+        if (objects[i].obj == obj) {
+            isFound = i;
+            break;
+        }
+    }
+
+    if(isFound != -1) {
+        memmove(&objects[i], &objects[i+1], (count-1-i) * sizeof(objWrapper));
+        count--;
+    }
+}
+
+void unloadList() {
+    free(objects);
+    objects = NULL;
+    count = 0;
+    capacity = 4;
+}
+
+static int compare(const void *a, const void *b) {
+    objWrapper* itemA = (objWrapper*)a;
+    objWrapper* itemB = (objWrapper*)b;
+
+    float sumA = itemA->pos->x + itemA->pos->y;
+    float sumB = itemB->pos->x + itemB->pos->y;
+
+    if (sumA < sumB) return -1;
+    if (sumA > sumB) return 1;
+    return 0;
 }
--- src/game.h
+++ src/game.h
@@ -15,12 +15,14 @@
 
 typedef enum { RIGHT, LEFT, DOWN, UP } dir;
 typedef enum { GAME, GAMEOVER } gameState;
+typedef enum { SNAKE, SEGMENT, APPLE } objType; 
 
 typedef struct { Rectangle src, dest; } Sprite;
 typedef struct { Sprite sprite; Vector2 pos; } Apple;
 typedef struct Segment Segment;
 struct Segment { Vector2 pos; Segment *next, *prev; };
 typedef struct { Sprite sprite; Vector2 pos; dir dir; Segment *head, *tail; } Snake;
+typedef struct { Vector2* pos; objType type; void* obj; } objWrapper; // a generic wrapper struct used for sorting game objects 
 
 extern gameState currentState;
 extern Texture2D texture;
@@ -30,7 +32,12 @@ void updateGame();
 void drawGame();
 void updateGameover();
 void drawGameover();
-static void addTail();
-static void removeTail();
+static Vector2 isoToScreen(Vector2 pos);
+static void addSegment();
+static void removeSegment();
+static void addToList(void* newObj, objType type);
+static void removeFromList(void* obj);
+static int compare(const void *a, const void *b);
+void unloadList();
 
 #endif
